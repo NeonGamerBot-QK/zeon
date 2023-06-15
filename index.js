14820e01926b391869410a590b3002e1c5ff2e9f
Saahil <neon@saahild.com>
2023-06-14 22:54:32 -0400

    ths

--- index.js
+++ index.js
@@ -57,34 +57,35 @@ if(!allowed_repos.includes(context.payload.repository.html_url)) {
   app.log.info("not running on " + context.payload.repository.html_url)
   return;
 }
+// const branch = context.payload.pull_request. HOW AM I SUPPOSED TO GET THE BRANCh!
     // Probot API note: context.repo() => { username: 'hiimbex', repo: 'testing-things' }
-    const res = await context.octokit.repos.createDeployment(
-      context.repo({
-        ref: context.payload.pull_request.head.ref, // The ref to deploy. This can be a branch, tag, or SHA.
-        task: "deploy", // Specifies a task to execute (e.g., deploy or deploy:migrations).
-        auto_merge: true, // Attempts to automatically merge the default branch into the requested ref, if it is behind the default branch.
-        required_contexts: [], // The status contexts to verify against commit status checks. If this parameter is omitted, then all unique contexts will be verified before a deployment is created. To bypass checking entirely pass an empty array. Defaults to all unique contexts.
-        payload: {
-          schema: "rocks!"
-        }, // JSON payload with extra information about the deployment. Default: ""
-        environment: "development", // Name for the target deployment environment (e.g., production, staging, qa)
-        description: "My Probot App's first deploy!", // Short description of the deployment
-        transient_environment: false, // Specifies if the given environment is specific to the deployment and will no longer exist at some point in the future.
-        production_environment: false // Specifies if the given environment is one that end-users directly interact with.
-      })
-    );
+    // const res = await context.octokit.repos.createDeployment(
+    //   context.repo({
+    //     ref: context.payload.pull_request.head.ref, // The ref to deploy. This can be a branch, tag, or SHA.
+    //     task: "deploy", // Specifies a task to execute (e.g., deploy or deploy:migrations).
+    //     auto_merge: true, // Attempts to automatically merge the default branch into the requested ref, if it is behind the default branch.
+    //     required_contexts: [], // The status contexts to verify against commit status checks. If this parameter is omitted, then all unique contexts will be verified before a deployment is created. To bypass checking entirely pass an empty array. Defaults to all unique contexts.
+    //     payload: {
+    //       schema: "rocks!"
+    //     }, // JSON payload with extra information about the deployment. Default: ""
+    //     environment: "development", // Name for the target deployment environment (e.g., production, staging, qa)
+    //     description: "My Probot App's first deploy!", // Short description of the deployment
+    //     transient_environment: false, // Specifies if the given environment is specific to the deployment and will no longer exist at some point in the future.
+    //     production_environment: false // Specifies if the given environment is one that end-users directly interact with.
+    //   })
+    // );
 
-    const deploymentId = res.data.id;
-    await context.octokit.repos.createDeploymentStatus(
-      context.repo({
-        deployment_id: deploymentId,
-        state: "success", // The state of the status. Can be one of error, failure, inactive, pending, or success
-        log_url: "https://saahild.com/pr/" + Math.random(), // The log URL to associate with this status. This URL should contain output to keep the user updated while the task is running or serve as historical information for what happened in the deployment.
-        description: "My Probot App set a deployment status!", // A short description of the status.
-        environment_url: "https://example.com/" + Math.random(), // Sets the URL for accessing your environment.
-        auto_inactive: true // Adds a new inactive status to all prior non-transient, non-production environment deployments with the same repository and environment name as the created status's deployment. An inactive status is only added to deployments that had a success state.
-      })
-    );
+  //   const deploymentId = res.data.id;
+  //   await context.octokit.repos.createDeploymentStatus(
+  //     context.repo({
+  //       deployment_id: deploymentId,
+  //       state: "success", // The state of the status. Can be one of error, failure, inactive, pending, or success
+  //       log_url: "https://saahild.com/pr/" + Math.random(), // The log URL to associate with this status. This URL should contain output to keep the user updated while the task is running or serve as historical information for what happened in the deployment.
+  //       description: "My Probot App set a deployment status!", // A short description of the status.
+  //       environment_url: "https://example.com/" + Math.random(), // Sets the URL for accessing your environment.
+  //       auto_inactive: true // Adds a new inactive status to all prior non-transient, non-production environment deployments with the same repository and environment name as the created status's deployment. An inactive status is only added to deployments that had a success state.
+  //     })
+  //   );
   });
 
   // For more information on building apps:
