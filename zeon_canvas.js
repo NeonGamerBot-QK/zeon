49b7c300fd6065ba34f2a9bcfcd497d687fec3b8
Saahil <neon@saahild.com>
2023-11-02 00:56:27 +0000

    e

--- zeon_canvas.js
+++ zeon_canvas.js
@@ -143,7 +143,7 @@ ${previews.map((p) => {
         fs.writeFileSync(fileName, fdata)
         try {
           const str = require('child_process').execSync('npx --yes jest --verbose zeon_canvas_file.test.js ' + path.basename(file.filename)).toString()
-          console.log(str)
+          app.log(str)
           ctx.octokit.issues.createComment(
     ctx.issue({ body: `# ✅ Test results \`${file.filename}\`:\n 
     \`\`\`
