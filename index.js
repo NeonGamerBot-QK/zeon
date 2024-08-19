cfc616433e267ab4b2ff5a87811f2040efe7193c
Saahil <saahilattud@gmail.com>
2024-08-19 15:06:49 -0400

    log

--- index.js
+++ index.js
@@ -378,7 +378,24 @@ I require pull request titles to follow the [Conventional Commits specification]
       }
     });
   });
+  app.on(["push"], async (ctx) => {
+    const context = ctx;
+    if (ctx.payload.pusher.name.includes("zeon")) return; // ignore my commitss
+    const push = ctx.payload;
+    console.log(push)
+    if (null) {
+      const compare = await ctx.octokit.repos.compareCommits(
+        context.repo({
+          base: push.before,
+          head: push.after,
+        }),
+      );
+  
+      const branch = push.ref.replace("refs/heads/", "");
+      compare.data.files.forEach(async (file) => { })
 
+  }
+  })
   app.on(["push"], async (ctx) => {
     const context = ctx;
     if (ctx.payload.pusher.name.includes("zeon")) return; // ignore my commitss
